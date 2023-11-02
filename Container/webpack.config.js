9a9847dc09fc9292f0f640f73cbaa7cc56d8ad3f
reyhaneh <reyhanehrm333@gmail.com>
2023-11-03 02:45:53 +0330

    init commit-create product and container structure

---
+++ Container/webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
---
+++ Products/webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
